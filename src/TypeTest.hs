a0d35870483685833d4fb6da2602d13a38987720
Callum Rogers <callumrogers@hotmail.co.uk>
2015-06-25 22:45:58 +0100

    Add hspec

--- src/TypeTest.hs
+++ src/TypeTest.hs
@@ -3,12 +3,17 @@
 module TypeTest where
 
 import Control.Exception
+import Test.Hspec
 
-a = "cat" :: Int
-
-test :: IO String
-test = do
+thing :: Show a => a -> IO String
+thing a = do
   result <- try (evaluate a)
   return $ case result of
     Right v -> "It succeeded with value " ++ show v
     Left e -> "It failed:\n" ++ show (e :: SomeException)
+
+test :: IO ()
+test = hspec $ do
+  describe "lol" $ do
+    it "should not let me make a string an int" $ do
+      thing ("cat" :: Int) >>= (`shouldBe` "cat")
