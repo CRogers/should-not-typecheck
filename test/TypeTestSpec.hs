ec83da989b8059ddcccea954a5beacd5538d3b4c
Callum Rogers <callumrogers@hotmail.co.uk>
2015-06-26 23:43:12 +0100

    Test that it propagates not HUnitFailure exceptions

--- test/TypeTestSpec.hs
+++ test/TypeTestSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE PatternSynonyms #-}
 {-# OPTIONS_GHC -fdefer-type-errors #-}
 
 module Main where
@@ -8,6 +9,10 @@ import Test.Hspec.Expectations (expectationFailure)
 import Test.HUnit.Lang (performTestCase)
 import TypeTest
 
+pattern TestCaseSuccess = Nothing
+pattern TestCaseFailure msg = Just (True, msg)
+pattern TestCaseError msg = Just (False, msg)
+
 shouldFailAssertion :: IO () -> IO ()
 shouldFailAssertion value = do
   result <- performTestCase value
@@ -15,6 +20,16 @@ shouldFailAssertion value = do
     Just (True, _) -> return ()
     Just (False, msg) -> expectationFailure $ "Raised an error " ++ msg
     Nothing -> expectationFailure "Did not throw an assertion error"
+
+shouldThrowException :: Exception e => e -> IO () -> IO ()
+shouldThrowException exception value = do
+  result <- performTestCase value
+  case result of
+    TestCaseSuccess -> expectationFailure "Test case succeeded, did not throw exception"
+    TestCaseFailure msg -> expectationFailure "Test case failed, did not throw exception"
+    TestCaseError msg -> case msg == show exception of
+      True -> return ()
+      False -> expectationFailure "Incorrect exception propagated"
 
 main :: IO ()
 main = hspec $ do
@@ -24,3 +39,7 @@ main = hspec $ do
 
     it "should throw an assertion error when an expression is well typed" $ do
       shouldFailAssertion (shouldNotCompile ("foo" :: String))
+
+    it "should throw an actual exception and not fail the assertion if the expression contains an non-HUnitFailure exception" $ do
+      let exception = NoMethodError "lol"
+      shouldThrowException exception (shouldNotCompile (throw exception))
