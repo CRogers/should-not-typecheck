7ac60d6b8c9b59ff83becab32bf33e4f1daf646b
Callum Rogers <callumrogers@hotmail.co.uk>
2015-06-26 23:28:33 +0100

    Extract out function

--- test/TypeTestSpec.hs
+++ test/TypeTestSpec.hs
@@ -8,6 +8,14 @@ import Test.Hspec.Expectations (expectationFailure)
 import Test.HUnit.Lang (performTestCase)
 import TypeTest
 
+shouldFailAssertion :: IO () -> IO ()
+shouldFailAssertion value = do
+  result <- performTestCase value
+  case result of
+    Just (True, _) -> return ()
+    Just (False, msg) -> expectationFailure $ "Raised an error " ++ msg
+    Nothing -> expectationFailure "Did not throw an assertion error"
+
 main :: IO ()
 main = hspec $ do
   describe "Type Test" $ do
@@ -15,8 +23,4 @@ main = hspec $ do
       shouldNotCompile ("foo" :: Int)
 
     it "should throw an assertion error when an expression is well typed" $ do
-      result <- performTestCase (shouldNotCompile ("foo" :: String))
-      case result of
-        Just (True, _) -> return ()
-        Just (False, msg) -> expectationFailure $ "Raised an error " ++ msg
-        Nothing -> expectationFailure "Did not throw an assertion error"
+      shouldFailAssertion (shouldNotCompile ("foo" :: String))
