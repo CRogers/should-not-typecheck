0adb9d9c54123618ab4193dc5c95dc802b7f1264
Callum Rogers <callumrogers@hotmail.co.uk>
2015-06-27 14:21:22 +0100

    Rename to should-not-typecheck, use HUnit instead of requiring hspec

--- src/TypeTest.hs
+++ src/TypeTest.hs
@@ -4,13 +4,13 @@ module TypeTest (shouldNotCompile) where
 
 import Control.Exception (evaluate, try, throw, ErrorCall(..))
 import Data.List (isSuffixOf)
-import Test.Hspec.Expectations (Expectation, expectationFailure)
+import Test.HUnit.Lang (Assertion, assertFailure)
 
-shouldNotCompile :: forall a. a -> Expectation
+shouldNotCompile :: forall a. a -> Assertion
 shouldNotCompile a = do
   result <- try (evaluate a)
   case result of
-    Right _ -> expectationFailure "Expected expression to not compile but it did compile"
+    Right _ -> assertFailure "Expected expression to not compile but it did compile"
     Left (ErrorCall msg) -> case isSuffixOf "(deferred type error)" msg of
       True -> return ()
       False -> throw (ErrorCall msg)
